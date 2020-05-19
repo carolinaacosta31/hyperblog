17b264182c2fc7e64c4d1304164350048864c7f5
Carolina Acosta <carolina.acosta.m31@gmail.com>
2020-05-18 21:56:57 -0500

    Recupero el reto 3: mensaje multilinea

--- holaNombre.py
+++ holaNombre.py
@@ -1,7 +1,13 @@
-def hola_nombre(nombre):
-    print(f'hola, {nombre}')
+def hola_nombre(nombre, apellido):
+    print(f'hola, {nombre} {apellido}')
+
+def mensaje_multilinea():
+    print('Platzi cuenta con cursos de: \n Desarrollo e ingeniería \n' 
+    ' Negocios y emprendimiento \n Crecimiento profesional \n Desarrollo y UX')
 
 if __name__ == '__main__':
     
     nombre = input('Cuál es tu nombre?')
-    hola_nombre(nombre)
+    apellido = input('Cuál es tu apellido?')
+    hola_nombre(nombre, apellido)
+    mensaje_multilinea()
