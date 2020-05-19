0bc6461f5bbd6b8e0c8a1e4b77dc5103dc5afc1a
Carolina Acosta <carolina.acosta.m31@gmail.com>
2020-05-18 21:54:27 -0500

    Ensayo, vuelvo al reto 1

--- holaNombre.py
+++ holaNombre.py
@@ -1,13 +1,7 @@
-def hola_nombre(nombre, apellido):
-    print(f'hola, {nombre} {apellido}')
-
-def mensaje_multilinea():
-    print('Platzi cuenta con cursos de: \n Desarrollo e ingeniería \n' 
-    ' Negocios y emprendimiento \n Crecimiento profesional \n Desarrollo y UX')
+def hola_nombre(nombre):
+    print(f'hola, {nombre}')
 
 if __name__ == '__main__':
     
     nombre = input('Cuál es tu nombre?')
-    apellido = input('Cuál es tu apellido?')
-    hola_nombre(nombre, apellido)
-    mensaje_multilinea()
+    hola_nombre(nombre)
